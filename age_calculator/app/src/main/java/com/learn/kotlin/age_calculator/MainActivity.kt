bc87874d5b66638c51164afe01810e171a56eae5
Frontier154 <13106945+sshukla154@users.noreply.github.com>
2020-06-26 10:59:18 +0530

    26062020 - UI Changes

---
+++ age_calculator/app/src/main/java/com/learn/kotlin/age_calculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.learn.kotlin.age_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
+        }
+    }
+}
